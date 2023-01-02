464145d4552c516b3f12b12305955ca3f037c1d6
Evgeny Zislis <evgeny.zislis@gmail.com>
2023-01-02 19:31:37 +0200

    add prefix to logs

--- wm-log.h
+++ wm-log.h
@@ -9,12 +9,12 @@
 void logger(FILE* io, const char* fmt, ...);
 
 #ifdef DEBUG
-#	define LOG_DEBUG(pFormat, ...) { logger(stdout, pFormat, ##__VA_ARGS__); }
+#	define LOG_DEBUG(pFormat, ...) { logger(stdout, "DEBUG: "pFormat, ##__VA_ARGS__); }
 #else
 #	define LOG_DEBUG(pFormat, ...) {}
 #endif
 
-#define LOG_ERROR(pFormat, ...) { logger(stderr, pFormat, ##__VA_ARGS__); }
-#define LOG_FATAL(pFormat, ...) { logger(stderr, pFormat, ##__VA_ARGS__); running = 0; }
+#define LOG_ERROR(pFormat, ...) { logger(stderr, "ERROR: "pFormat, ##__VA_ARGS__); }
+#define LOG_FATAL(pFormat, ...) { logger(stderr, "FATAL: "pFormat, ##__VA_ARGS__); running = 0; }
 
 #endif
