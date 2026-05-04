2a092d8e74cd0d63192087032785f35e6e7c834d
Evgeny Zislis <evgeny.zislis@gmail.com>
2026-05-04 08:18:51 +0300

    use pkgs.lib.getExe for xvfb-run and x11vnc

--- flake.nix
+++ flake.nix
@@ -3,84 +3,88 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            bear
-            clang
-            clang-tools
-            gcc
-            gnumake
-            pkg-config
-            libxcb
-            libxcb-util
-            libxcb-keysyms
-            libxcb-wm
-            libxcb-errors
-            libx11
-            libxrandr
-            libxinerama
-            freetype
-            fontconfig
-            gdb
-          ];
+    { nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        { pkgs, system }:
+        {
+          packages."${system}".default = pkgs.stdenv.mkDerivation {
+            pname = "wm-xcb";
+            version = "0.0.1";
+            src = ./.;
+            buildInputs = with pkgs; [
+              libxcb
+              libxcb-util
+              libxcb-keysyms
+              libxcb-wm
+              libxcb-errors
+              libx11
+              libxrandr
+              libxinerama
+              freetype
+              fontconfig
+            ];
+            propagatedBuildInputs = with pkgs; [
+              xvfb-run
+              x11vnc
+            ];
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              gnumake
+            ];
+            buildPhase = "make";
+            installPhase = ''
+              mkdir -p $out/bin
+              cp wm $out/bin/
+              cat > $out/bin/start.sh << EOF
+              #!/usr/bin/env sh
+              ${pkgs.lib.getExe pkgs.xvfb-run} ${pkgs.lib.getExe pkgs.x11vnc} -display :99 -forever -shared &
+              sleep 2
+              DISPLAY=:99 /wm/bin/wm
+              EOF
+              chmod +x $out/bin/start.sh
+            '';
+          };
 
-          shellHook = ''
-            echo "wm-xcb development environment loaded"
-          '';
+          devShells."${system}".default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              bear
+              clang
+              clang-tools
+              gcc
+              gnumake
+              pkg-config
+              libxcb
+              libxcb-util
+              libxcb-keysyms
+              libxcb-wm
+              libxcb-errors
+              libx11
+              libxrandr
+              libxinerama
+              freetype
+              fontconfig
+              gdb
+            ];
+
+            shellHook = ''
+              echo "wm-xcb development environment loaded"
+            '';
+          };
         };
 
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "wm-xcb";
-          version = "0.0.1";
-          src = ./.;
-          buildInputs = with pkgs; [
-            libxcb
-            libxcb-util
-            libxcb-keysyms
-            libxcb-wm
-            libxcb-errors
-            libx11
-            libxrandr
-            libxinerama
-            freetype
-            fontconfig
-          ];
-          propagatedBuildInputs = with pkgs; [
-            xvfb
-            x11vnc
-          ];
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            gnumake
-          ];
-          buildPhase = ''
-            make
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            cp wm $out/bin/
-            # Create startup script that launches Xvfb, x11vnc, and wm
-            cat > $out/bin/start.sh << STARTSCRIPT
-#!/bin/sh
-${pkgs.xvfb}/bin/Xvfb :0 -screen 0 1024x768x24 &
-${pkgs.x11vnc}/bin/x11vnc -display :0 -forever -shared &
-sleep 1
-DISPLAY=:0 /wm/bin/wm
-STARTSCRIPT
-            chmod +x $out/bin/start.sh
-          '';
-        };
-      }
-    );
+      outputsForSystem =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        perSystem { inherit pkgs system; };
+
+      combinedOutputs = builtins.foldl' (acc: system: acc // outputsForSystem system) { } systems;
+    in
+    combinedOutputs;
 }
